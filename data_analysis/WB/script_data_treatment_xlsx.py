9b5ef1085394745e862acdb569ed953407d7dc66
Ana Lima <ayf.lima@usp.br>
2023-04-10 08:42:21 -0300

    feat: Add data treatment script to World Bank analysis

---
+++ data_analysis/WB/script_data_treatment_xlsx.py
@@ -0,0 +1,25 @@
+import time
+import sys
+sys.path.append('../../data_treatment')
+sys.path.append('./data_treatment')
+import os
+from modules.DataTreatment import DataTreatment
+
+start = time.time()
+
+if 'multimodal' in os.getcwd():
+    data_folder = '../../data/'
+elif 'data_analysis' in os.getcwd():
+    data_folder = '../data/'
+else:
+    data_folder = './data/'
+
+source_folder_path = data_folder + 'WB/'
+destination_folder_path = data_folder + 'WB/'
+
+dt = DataTreatment(source_folder_path, destination_folder_path)
+dt.transform_xlsx_to_csv_and_copy_to_destination_folder()
+
+end = time.time()
+
+print("Time to complete Data Treatment: ", end-start)
