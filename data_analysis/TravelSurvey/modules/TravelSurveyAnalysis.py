9bb1dbad4c15a0089c041e98adb73a839568ca5c
Ana Lima <ayf.lima@usp.br>
2023-10-12 17:09:25 +0200

    feat: Improve modules functionalities in class TravelSurveyAnalysis

--- data_analysis/TravelSurvey/modules/TravelSurveyAnalysis.py
+++ data_analysis/TravelSurvey/modules/TravelSurveyAnalysis.py
@@ -36,12 +36,12 @@ class TravelSurveyAnalysis:
     def get_specific_mode_trips(self, df_trips, mode_column, filter_list):
         '''
             filter is a list with the values of the mode_column
-            that should be included in the 
+            that should be included in the output
         '''
         filter_trips = df_trips[(df_trips[mode_column].isin(filter_list))]
         return filter_trips
     
-    def calculate_distribution(self, df, variable_column, expansion_factor, index_map = {}):
+    def calculate_distribution(self, df, variable_column, expansion_factor, index_map = {}, normalize=True):
         '''
             the parameter "expansion_factor" is either False (boolean)
             or a string with the column that contains the expansion factor
@@ -57,11 +57,18 @@ class TravelSurveyAnalysis:
             df_grouped  = df.groupby(new_variable_column)[expansion_factor].sum()
             if index_map != {}:
                 df_grouped = df_grouped[df_grouped.index.isin(list(index_map.values()))]
-            df_percentage = df_grouped / df_grouped.sum() * 100
+            if normalize: # if normalize is True, calculate the percentage. Otherwise return the values.
+                df_percentage = df_grouped / df_grouped.sum() * 100
+            else:
+                df_percentage = df_grouped
         else: # expansion_factor is False. Simply calculate the distribution in the column
-            df_percentage = df[new_variable_column].value_counts(normalize=True) * 100
+            df_percentage = df[new_variable_column].value_counts(normalize=normalize) * 100
         
         df_percentage = pd.DataFrame(df_percentage)
+
+        for value in index_map.values():
+            if value and value not in df_percentage.index:
+                    df_percentage.loc[value] = 0
 
         return df_percentage
 
@@ -95,7 +102,7 @@ class TravelSurveyAnalysis:
         
         ax.set_xticks(list_bins)
 
-        # ax.set_ylim(bottom = 0, top = 35)
+        ax.set_ylim(bottom = 0, top = 20)
 
         ax.axes.set_title('Age distribution for ' + mode, fontsize=30, pad = 15)
         ax.set_xlabel('Age (years)',fontsize=25, labelpad = 15)
@@ -106,9 +113,11 @@ class TravelSurveyAnalysis:
         plt.show()
 
         if save:
-            ax.figure.savefig(self.destination_folder_path + 'charts/histograms/' + variable_column + '_expand_' + expansion_factor, bbox_inches='tight')
+            fig_filename = variable_column + '_expand_' + str(expansion_factor)+ '_' + mode 
+            fig_filename = fig_filename.replace(' ', '_')
+            ax.figure.savefig(self.destination_folder_path + 'charts/histograms/' + fig_filename, bbox_inches='tight')
     
-    def plot_percentage_class(self, data, social_class_column, index_map, bike = False):
+    def plot_percentage_class(self, data, social_class_column, index_map, bike = False, save = False):
         df = self.calculate_distribution(data, social_class_column, self.expansion_factor_person, index_map)
         plt.figure(figsize=(22, 11))
         ax = df[self.expansion_factor_person].plot(kind='pie', autopct='%1.1f%%', fontsize = 20, ylabel=None)
@@ -117,17 +126,27 @@ class TravelSurveyAnalysis:
             ax.set_title('Percentage of each class for bike trips', fontsize = 25)
         else:
             ax.set_title('Percentage of each class for all trips', fontsize = 25)
+        
+        if save:
+            fig_filename = 'percentage_class_' + social_class_column + '_expand_' + str(self.expansion_factor_person)
+            if bike:
+                fig_filename = fig_filename + '_bike'
+            else:
+                fig_filename = fig_filename + '_all_modes'
 
-    def plot_mode_separated_by_class(self, data, social_class_column, mode_column, mode_index_map, classes_index_map):
+            fig_filename = fig_filename.replace(' ', '_')
+            ax.figure.savefig(self.destination_folder_path + 'charts/pies/' + fig_filename, bbox_inches='tight')
+    
+    def plot_mode_separated_by_class(self, data, social_class_column, mode_column, mode_index_map, classes_index_map, save=False):
 
-        nrow=2
-        ncol=3
+        nrow=3
+        ncol=2
         classes = {1: 'A', 2:'B1', 3:'B2', 4: 'C1', 5:'C2', 6:'D-E'}
 
         fig, axes = plt.subplots(nrow, ncol)
 
         fig.patch.set_facecolor('white')
-        fig.set_size_inches(18.5, 11)
+        fig.set_size_inches(11, 18.5)
         fig.suptitle('Percentage of each mode of transportation, separated by class', fontsize = 25)
 
         count = 1
@@ -140,31 +159,43 @@ class TravelSurveyAnalysis:
         for r in range(nrow):
             for c in range(ncol):
                 social_class = classes[count]
-        
-                data_class = data[data[new_variable_column] == social_class]
-
-                data_plot = self.calculate_distribution(data_class, mode_column, self.expansion_factor_trip, mode_index_map)
-
-                colors = ['sandybrown', 'limegreen', 'cornflowerblue','hotpink', 'darkorchid']
-                    
-                if count == 1:
-                    data_plot.plot(ax=axes[r,c], kind='pie', autopct='%1.1f%%', fontsize = 15, colors = colors, y=self.expansion_factor_trip)
+                
+                if int(percentages_social_class.loc[social_class]) == 0:
+                    axes[r,c].axis('equal')  # Set aspect ratio to make it a circle
+                    axes[r,c].pie([1], labels=[''], autopct='%1.1f%%', colors = colors)
                 else:
-                    data_plot.plot(ax=axes[r,c],  labels = None, kind='pie', autopct='%1.1f%%', fontsize = 15, colors = colors, y=self.expansion_factor_trip)
+                    data_class = data[data[new_variable_column] == social_class]
 
+                    data_plot = self.calculate_distribution(data_class, mode_column, self.expansion_factor_trip, mode_index_map)
+
+                    colors = ['sandybrown', 'limegreen', 'cornflowerblue','hotpink', 'darkorchid']
+
+                    if count == 1:
+                        data_plot.plot(ax=axes[r,c], kind='pie', autopct='%1.1f%%', fontsize = 15, colors = colors, y=self.expansion_factor_trip)
+                    else:
+                        data_plot.plot(ax=axes[r,c],  labels = None, kind='pie', autopct='%1.1f%%', fontsize = 15, colors = colors, y=self.expansion_factor_trip)
+                    axes[r,c].get_legend().remove()
+                
                 percentage_social_class = round(float(percentages_social_class.loc[social_class]),1)
                 axes[r,c].set_title('Class ' + social_class +
                                     ': ' + str(percentage_social_class) + '%', fontsize = 15)
-                            
-                axes[r,c].get_legend().remove()
-                
+                                
+                    
                 axes[r,c].set(ylabel=None)
                 count+=1
+        
+        plt.subplots_adjust(right=1)
+
+        if save:
+            fig_filename = 'mode_per_class_' + social_class_column + '_expand_' + str(self.expansion_factor_person)
+            fig_filename = fig_filename.replace(' ', '_')
+            fig.savefig(self.destination_folder_path + 'charts/pies/' + fig_filename, bbox_inches='tight')
+
 
     def plot_duration_separated_by_class(self, data, duration_column, social_class_column,
-                                         classes_index_map, bike=False, expansion_factor = True):
-        nrow=2
-        ncol=3
+                                         classes_index_map, bike=False, expansion_factor = True, save = False):
+        nrow=3
+        ncol=2
 
         classes = {0: 'NA', 1: 'A', 2:'B1', 3:'B2', 4: 'C1', 5:'C2', 6:'D-E'}
         # classes = {1: 'A', 2:'B1', 3:'B2', 4: 'C1', 5:'C2', 6:'D-E'}
@@ -172,7 +203,7 @@ class TravelSurveyAnalysis:
         fig, axes = plt.subplots(nrow, ncol)
 
         fig.patch.set_facecolor('white')
-        fig.set_size_inches(18.5, 11)
+        fig.set_size_inches(11, 16)
         if bike:
             fig.suptitle('Duration of bicycle trips, separated by class', fontsize = 25)
         else:
@@ -209,3 +240,13 @@ class TravelSurveyAnalysis:
                 stepsize = 20
                 axes[r,c].yaxis.set_ticks(np.arange(start, end+1, stepsize))
                 count+=1
+
+        if save:
+            fig_filename = 'duration_per_class_' + social_class_column + '_expand_' + str(self.expansion_factor_person)
+            if bike:
+                fig_filename = fig_filename + '_bike'
+            else:
+                fig_filename = fig_filename + '_all_modes'
+            fig_filename = fig_filename.replace(' ', '_')
+            fig.savefig(self.destination_folder_path + 'charts/boxplots/' + fig_filename, bbox_inches='tight')
+
